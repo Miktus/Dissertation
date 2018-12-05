4c0f72fb717d8780f1e10abb2f8e6139eeefaaa9
miktus <michal.miktus@gmail.com>
2018-12-05 20:44:28 +0100

    Add VAR estimation on simulated observations

--- VAR_estimation/Basic_VAR_031218.py
+++ VAR_estimation/Basic_VAR_031218.py
@@ -5,7 +5,7 @@
 
 # ## Libraries
 
-# In[9]:
+# In[1]:
 
 
 import numpy as np
@@ -13,7 +13,9 @@ import pandas as pd
 import statsmodels.api as sm
 from statsmodels.tsa.api import VAR, DynamicVAR
 from statsmodels.tsa.base.datetools import dates_from_str
+from statsmodels.tsa.vector_ar.vecm import coint_johansen
 import matplotlib
+from random import random
 
 
 # ## Importing data
@@ -22,6 +24,7 @@ import matplotlib
 
 
 # Importing data
+
 mdata = sm.datasets.macrodata.load_pandas().data
 mdata.head()
 
@@ -30,21 +33,43 @@ mdata.head()
 
 
 # Basic data manipulation
+
 dates = mdata[['year', 'quarter']].astype(int).astype(str)
 quarterly = dates["year"] + "Q" + dates["quarter"]
 quarterly = dates_from_str(quarterly)
+
 mdata = mdata[['realgdp','realcons','realinv']]
+
+# Pandas object to a time-series model expects that the index is dates
+
 mdata.index = pd.DatetimeIndex(quarterly)
 data = np.log(mdata).diff().dropna()
 data.head()
 
 
-# ## VAR
-
 # In[4]:
 
 
+# Plot the data 
+
+data.plot(subplots=True,figsize=(18, 16));
+
+
+# In[5]:
+
+
+# Checking stationarity
+
+coint_johansen(data,-1,1).eig
+
+
+# ## VAR
+
+# In[6]:
+
+
 # VAR
+
 model = VAR(data)
 results = model.fit(2)
 results.summary()
@@ -52,7 +77,7 @@ results.summary()
 
 # ## Plots
 
-# In[11]:
+# In[7]:
 
 
 # Results
@@ -60,16 +85,103 @@ results.summary()
 results.plot();
 
 
-# In[12]:
+# In[8]:
 
 
-# Autocrrelation
+# Autocorrelation
 
 results.plot_acorr();
 
 
-# In[ ]:
+# ## Lag order selection
+
+# In[9]:
 
 
+# Adding maximum lag
 
+model.select_order(5)
+
+# Estimate model with maximum lag according to the AIC criterion
+
+results = model.fit(maxlags=5, ic='aic')
+results.summary()
+
+
+# ## Forecasting
+
+# In[10]:
+
+
+lag_order = results.k_ar
+
+# Specify the initial value of forecast
+
+results.forecast(data.values[-lag_order:], 5);
+
+# Plot forecasts with confidence intervals
+
+results.plot_forecast(10);
+
+
+# ## Impulse Response Analysis
+
+# In[11]:
+
+
+# IRF
+
+irf = results.irf(10)
+irf.plot(orth=False);
+
+
+# In[12]:
+
+
+# Cumulated IRF
+
+irf.plot_cum_effects(orth=False);
+
+
+# ## Forecast Error Variance Decomposition (FEVD)
+
+# In[13]:
+
+
+fevd = results.fevd(5)
+
+fevd.summary()
+
+
+# In[14]:
+
+
+#Plot FEVD
+
+results.fevd(20).plot();
+
+
+# # Simulated part
+
+# In[15]:
+
+
+data_sim = data.copy()
+
+for i in range(0,len(data_sim.columns)):
+    data_sim.iloc[:,i] = random()
+
+
+# In[16]:
+
+
+model_sim = VAR(data)
+results_sim = model_sim.fit(2)
+results_sim.summary()
+
+
+# In[17]:
+
+
+results_sim.plot();
 
